31f4a3609147c1a82bac31ea56a9ccc5792a8e38
Lacctoff <lasisitofunmi@gmail.com>
2025-10-12 00:52:33 +0100

    feat: added Figma icons for dashboard

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import "../../styles/main.scss";
+import "../../styles/dashboard.scss";
+import Sidebar from "@/components/Sidebar";
+import Header from "@/components/Header";
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="dashboard-layout">
+      <Sidebar />
+      <div className="dashboard-content">
+        <Header/>
+        <main className="dashboard-main">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+}
